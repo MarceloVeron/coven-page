4772f45ad302b29f8d5d96d26e600e5b47d769f6
Marcelo Veron <marceloveron76@gmail.com>
2021-08-24 20:17:53 -0300

    vs-home no editable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,26 @@
 @extends('plantilla')
 
 @section('seccion')
-<nav>
-    <div class="row">
-        <div class="col-12 ">
-            <div class="row d-flex">
-                <div class="col-10">
-                    icono
-                </div>
-                <div class="col-2">
-                    seccion
-                </div>
-            </div>
-           
+<section class="content">
+        <div class="home">
+            <img class="home-img" alt="">
         </div>
-    </div>
-</nav>
-@foreach ( $texts as $txt)
-<h1>{{$txt->txt_header}}</h1>
+    </section>
+    <section class="content_text">
+        <div class="content_home_text">
+            <h3>Yo <span class="text_home">Cambio</span> el mundo. Soy una <span class="text_home">ingeniera</span></h3>
+            <span class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo praesentium error quod maiores quam. Accusantium ipsam reprehenderit at fugiat quia illum totam deserunt iusto fugit architecto minus, similique vitae soluta!</span>
+            <span class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
+            <span class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus, nemo repellat fugiat sapiente ad dignissimos corporis</span>
+            <span class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo a, iusto reprehenderit harum aliquid mollitia eos nesciunt fuga earum dolor </span>
+        </div>
+    </section>
 
-@endforeach
+            @foreach ( $texts as $txt)
+            <h1>{{$txt->txt_header}}</h1>
+            @endforeach
+
+</div>
+
 
 @endsection
